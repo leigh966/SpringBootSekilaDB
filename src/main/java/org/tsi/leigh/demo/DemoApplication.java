63885ddc4d7b98fea2d5fa3369f182c0bd1d28ef
leigh966 <leigh966@hotmail.com>
2022-02-08 15:41:28 +0000

    Another try#

--- src/main/java/org/tsi/leigh/demo/DemoApplication.java
+++ src/main/java/org/tsi/leigh/demo/DemoApplication.java
@@ -15,7 +15,8 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name)
+	{
 
 		return String.format("Hello %s!", name);
 
